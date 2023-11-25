3c8d0dd012ee4c23d7f57eb30b846176e3742980
Adit Mohan Bakshi <93762469+Adluski@users.noreply.github.com>
2023-11-25 19:10:29 +0530

    Update vcap.py

--- vcap.py
+++ vcap.py
@@ -10,23 +10,23 @@ class SphericalCapVolume(ThreeDScene):
         # Create the sphere with radius R
         sphere = Sphere(radius=R).set_opacity(0.5).set_color(BLUE)
         
-        # Create the spherical cap
-        cap = Hemisphere(radius=R).set_opacity(0.5).set_color(RED)
-        cap_bottom = Circle(radius=a).shift(DOWN*(R-h))
-        cap.add_to_back(cap_bottom)
+        # Create a hemisphere by scaling a sphere
+        hemisphere = Sphere(radius=R, u_min=PI/2, u_max=PI).set_opacity(0.5).set_color(RED)
+        hemisphere_bottom = Circle(radius=a).shift(DOWN*(R-h))
+        hemisphere.add_to_back(hemisphere_bottom)
 
-        # Position the cap correctly within the sphere
-        cap.shift(UP*h)
+        # Position the hemisphere correctly within the sphere as a cap
+        hemisphere.shift(UP*h)
 
         # Text and formulas to display
         volume_text = MathTex(r"V = \frac{1}{3}\pi h^2 (3R - h)").to_corner(UL)
         volume_formula_crater = MathTex(r"V = \frac{1}{6}\pi h (3a^2 + h^2)").next_to(volume_text, DOWN)
 
-        # Animate the sphere and cap creation
+        # Animate the sphere and hemisphere creation
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
         self.begin_ambient_camera_rotation(rate=0.1)  # Slowly rotate the camera
         self.add(sphere)
-        self.play(ShowCreation(cap))
+        self.play(ShowCreation(hemisphere))
         self.wait(2)
 
         # Show the volume formulas
@@ -34,7 +34,7 @@ class SphericalCapVolume(ThreeDScene):
         self.wait(2)
 
         # Animate the equivalence of the volumes
-        self.play(Transform(cap, sphere))
+        self.play(Transform(hemisphere, sphere))
         self.wait(2)
 
         # Remove the formulas and stop the rotation
@@ -43,7 +43,7 @@ class SphericalCapVolume(ThreeDScene):
         self.wait(2)
         
         # Clear the scene
-        self.play(FadeOut(sphere), FadeOut(cap))
+        self.play(FadeOut(sphere), FadeOut(hemisphere))
 
 # To run this scene, use the following command in your terminal:
-# manim -pql <scriptname>.py SphericalCapVolume
+# manim -pql vcap.py SphericalCapVolume
