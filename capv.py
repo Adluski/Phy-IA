a820114f0b68d01a69617cf553182fb963bcbf43
Adit Mohan Bakshi <93762469+Adluski@users.noreply.github.com>
2023-11-26 00:15:09 +0530

    Update capv.py

--- capv.py
+++ capv.py
@@ -2,27 +2,46 @@ from manim import *
 
 class SphericalCapVolume(ThreeDScene):
     def construct(self):
-        # Define the sphere and spherical cap parameters
-        R = 3  # Radius of the sphere
-        h = 1  # Height of the spherical cap
-        a = (R**2 - (R - h)**2)**0.5  # Radius of the base of the spherical cap
+        # Constants
+        R = 2  # Radius of the sphere
+        h_max = R  # Maximum height of the cap
 
-        # Create the sphere
-        sphere = Sphere(radius=R).set_color(WHITE).set_opacity(0.5)
-
-        # Create the circle to represent the base of the spherical cap
-        cap_base = Circle(radius=a).set_fill(RED, opacity=0.5).move_to(UP*(R - h))
-
-        # Display text and formulas
-        volume_text = MathTex(r"V = \frac{1}{3}\pi h^2 (3R - h)", color=WHITE).to_edge(UP)
-        volume_formula = MathTex(r"V = \frac{1}{6}\pi h (3a^2 + h^2)", color=WHITE).next_to(volume_text, DOWN)
-
-        # Add the sphere and the cap to the scene
-        self.add(sphere)
-        self.add(cap_base)
-        self.add(volume_text)
-        self.add(volume_formula)
-
-        # Set the camera orientation
+        # Create a sphere
+        sphere = Sphere(radius=R, resolution=(30, 30))
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.wait(5)
+        self.add(sphere)
+
+        # Create a cap
+        cap_height = ValueTracker(0.1)
+        cap = always_redraw(lambda: self.get_spherical_cap(R, cap_height.get_value()))
+
+        # Height line
+        height_line = always_redraw(lambda: Line(
+            start=sphere.get_bottom(),
+            end=sphere.get_bottom() + cap_height.get_value() * UP,
+            color=YELLOW
+        ))
+
+        # Volume formula
+        volume_formula = always_redraw(lambda: MathTex(
+            "V = \\frac{1}{3} \\pi ", "{h}", "^2", "(3R - ", "{h}", ")",
+            substrings_to_isolate="h"
+        ).set_color_by_tex("h", YELLOW).next_to(height_line, RIGHT))
+
+        # Animate
+        self.add(cap, height_line, volume_formula)
+        self.play(cap_height.animate.set_value(h_max), run_time=4, rate_func=there_and_back)
+        self.wait()
+
+    def get_spherical_cap(self, R, h):
+        # Function to create a spherical cap
+        return ParametricSurface(
+            lambda u, v: np.array([
+                R * np.sin(u) * np.cos(v),
+                R * np.sin(u) * np.sin(v),
+                R * np.cos(u)
+            ]),
+            u_range=[np.arccos(1 - h/R), PI],
+            v_range=[0, TAU],
+            resolution=(20, 20)
+        ).set_color(RED)
