31c6a547b9ffd4fae7c8eccf6f97411d70967ac9
Adit Mohan Bakshi <93762469+Adluski@users.noreply.github.com>
2023-11-26 00:57:13 +0530

    Update spherevolume.py

--- spherevolume.py
+++ spherevolume.py
@@ -2,31 +2,32 @@ from manim import *
 
 class SV(ThreeDScene):
     def construct(self):
-        # Define the radius of the sphere
-        radius = 2
+        radius = 2  # Radius of the sphere
+        height = 2 * radius  # Height is twice the radius
+
+        # Function to calculate y (radius of the disk at height h)
+        def y(h):
+            return np.sqrt(radius**2 - (h - radius)**2)
 
         # Create a sphere
         sphere = Sphere(radius=radius, resolution=(101, 51))
-        sphere.set_color(BLUE_E)
-        sphere.set_opacity(0.5)
+        sphere.set_color(RED_E)
 
-        # Create a cube with side length 2r (same as sphere diameter)
-        cube = Cube(side_length=2 * radius)
-        cube.set_color(RED_E)
-        cube.set_opacity(0.5)
-        cube.move_to(sphere.get_center())
+        # Add the sphere to the scene
+        self.add(sphere)
 
-        # Position the sphere and cube side by side
-        group = Group(sphere, cube)
-        group.arrange(RIGHT, buff=1)
+        # Create disks
+        disks = VGroup()
+        for h in np.linspace(0, height, 50):
+            disk_radius = y(h)
+            disk = Circle(radius=disk_radius)
+            disk.set_stroke(width=0)
+            disk.set_fill(BLUE_E, opacity=0.5)
+            disk.move_to(sphere.get_center() + UP * (h - radius))
+            disks.add(disk)
 
-        # Add the objects to the scene
-        self.add(group)
-
-        # Rotate the group to show 3D perspective
-        self.move_camera(phi=45 * DEGREES, theta=45 * DEGREES)
-        self.wait(2)
-
-        # Rotate the objects for better view
-        self.play(Rotate(group, angle=PI, axis=UP))
+        # Animate the construction of the sphere from the disks
+        self.move_camera(phi=75 * DEGREES, theta=-45 * DEGREES)
+        self.begin_ambient_camera_rotation(rate=0.1)
+        self.play(Create(disks), run_time=5)
         self.wait(2)
