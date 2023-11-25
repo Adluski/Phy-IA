68dd8eba13335dc3ff7b8b61ac86dfa9e0e6f0cc
Adit Mohan Bakshi <93762469+Adluski@users.noreply.github.com>
2023-11-25 19:06:14 +0530

    Update vcap.py

--- vcap.py
+++ vcap.py
@@ -1,39 +1,49 @@
 from manim import *
 
-class SphericalCapVolume(Scene):
+class SphericalCapVolume(ThreeDScene):
     def construct(self):
-        # Parameters for the sphere and cap
-        sphere_radius = 2
-        cap_height = ValueTracker(1)
-
-        # Sphere
-        sphere = Sphere(radius=sphere_radius, resolution=(101, 51)).set_opacity(0.5)
+        # Parameters for the sphere and spherical cap
+        R = 3  # Radius of the sphere
+        h = 1  # Height of the spherical cap
+        a = (R**2 - (R - h)**2)**0.5  # Radius of the base of the spherical cap
         
-        # Cap - a cut portion of the sphere
-        cap = always_redraw(lambda: 
-            Intersection(
-                Sphere(radius=sphere_radius, resolution=(101, 51)),
-                Cube(side_length=2 * sphere_radius).shift(UP * (sphere_radius - cap_height.get_value() / 2)),
-            ).set_color(BLUE).set_opacity(0.75)
-        )
+        # Create the sphere with radius R
+        sphere = Sphere(radius=R).set_opacity(0.5).set_color(BLUE)
+        
+        # Create the spherical cap
+        cap = Hemisphere(radius=R).set_opacity(0.5).set_color(RED)
+        cap_bottom = Circle(radius=a).shift(DOWN*(R-h))
+        cap.add_to_back(cap_bottom)
 
-        # Volume formula text
-        volume_formula = always_redraw(lambda: 
-            MathTex("V = \\frac{\\pi h^2 (3R - h)}{3}", "=", 
-                f"{PI * cap_height.get_value()**2 * (3 * sphere_radius - cap_height.get_value()) / 3:.2f}")
-            .to_edge(UP)
-        )
+        # Position the cap correctly within the sphere
+        cap.shift(UP*h)
 
-        # Height value updater
-        height_label = always_redraw(lambda: 
-            MathTex("h =", f"{cap_height.get_value():.2f}")
-            .next_to(cap, UP)
-        )
+        # Text and formulas to display
+        volume_text = MathTex(r"V = \frac{1}{3}\pi h^2 (3R - h)").to_corner(UL)
+        volume_formula_crater = MathTex(r"V = \frac{1}{6}\pi h (3a^2 + h^2)").next_to(volume_text, DOWN)
 
-        # Animation
-        self.play(Create(sphere))
-        self.play(Write(volume_formula), GrowFromCenter(cap))
-        self.add(height_label)
-        self.wait(1)
-        self.play(cap_height.animate.set_value(sphere_radius), rate_func=there_and_back, run_time=4)
-        self.wait(1)
+        # Animate the sphere and cap creation
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.begin_ambient_camera_rotation(rate=0.1)  # Slowly rotate the camera
+        self.add(sphere)
+        self.play(ShowCreation(cap))
+        self.wait(2)
+
+        # Show the volume formulas
+        self.play(Write(volume_text), Write(volume_formula_crater))
+        self.wait(2)
+
+        # Animate the equivalence of the volumes
+        self.play(Transform(cap, sphere))
+        self.wait(2)
+
+        # Remove the formulas and stop the rotation
+        self.play(FadeOut(volume_text), FadeOut(volume_formula_crater))
+        self.stop_ambient_camera_rotation()
+        self.wait(2)
+        
+        # Clear the scene
+        self.play(FadeOut(sphere), FadeOut(cap))
+
+# To run this scene, use the following command in your terminal:
+# manim -pql <scriptname>.py SphericalCapVolume
