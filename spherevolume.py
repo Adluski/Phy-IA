dac78a88d10f8ea420a245ba206b0ff15f5c499f
Adit Mohan Bakshi <93762469+Adluski@users.noreply.github.com>
2023-11-26 00:47:06 +0530

    Update spherevolume.py

--- spherevolume.py
+++ spherevolume.py
@@ -1,29 +1,32 @@
 from manim import *
 
-class SphereVolume(ThreeDScene):
+class SV(ThreeDScene):
     def construct(self):
         # Define the radius of the sphere
         radius = 2
 
-        # Create a sphere with the specified radius
-        sphere = Sphere(radius=radius)
+        # Create a sphere
+        sphere = Sphere(radius=radius, resolution=(101, 51))
         sphere.set_color(BLUE_E)
-        sphere.set_gloss(0.8)
+        sphere.set_opacity(0.5)
 
-        # Calculate the volume of the sphere
-        volume = (4/3) * PI * radius**3
+        # Create a cube with side length 2r (same as sphere diameter)
+        cube = Cube(side_length=2 * radius)
+        cube.set_color(RED_E)
+        cube.set_opacity(0.5)
+        cube.move_to(sphere.get_center())
 
-        # Create a text object to display the volume
-        volume_text = Text(f"Volume of sphere: {volume:.2f} units³", font_size=36)
+        # Position the sphere and cube side by side
+        group = Group(sphere, cube)
+        group.arrange(RIGHT, buff=1)
 
-        # Position the text on the screen
-        volume_text.to_edge(UP)
+        # Add the objects to the scene
+        self.add(group)
 
-        # Add the sphere and the text to the scene
-        self.add(sphere, volume_text)
+        # Rotate the group to show 3D perspective
+        self.move_camera(phi=45 * DEGREES, theta=45 * DEGREES)
+        self.wait(2)
 
-        # Rotate the sphere
-        self.play(Rotate(sphere, angle=PI/2, axis=UP))
-
-        # Show the scene
+        # Rotate the objects for better view
+        self.play(Rotate(group, angle=PI, axis=UP))
         self.wait(2)
