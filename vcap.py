e8af40e21c1da0dd0f7d6f9adada11a09744d927
Adit Mohan Bakshi <93762469+Adluski@users.noreply.github.com>
2023-11-25 19:18:17 +0530

    Update vcap.py

--- vcap.py
+++ vcap.py
@@ -2,41 +2,35 @@ from manim import *
 
 class SphericalCapVolume(ThreeDScene):
     def construct(self):
-        # Parameters for the sphere and spherical cap
+        # Define the sphere and spherical cap parameters
         R = 3  # Radius of the sphere
         h = 1  # Height of the spherical cap
         a = (R**2 - (R - h)**2)**0.5  # Radius of the base of the spherical cap
+        
+        # Create the sphere
+        sphere = Sphere(radius=R).set_color(WHITE).set_opacity(0.2)
+        
+        # Create the circle to represent the base of the spherical cap
+        cap_base = Circle(radius=a).set_stroke(BLUE, opacity=0.8).move_to(UP*(R - h))
+        
+        # Create a filled circle to represent the spherical cap volume
+        cap_volume = Dot(point=UP*(R - h), radius=a).set_color(RED).set_opacity(0.5)
 
-        # Create the sphere with radius R
-        sphere = Sphere(radius=R).set_opacity(0.5).set_color(BLUE)
-
-        # Create the circle to simulate the spherical cap
-        cap = Circle(radius=a).set_fill(RED, opacity=0.5).set_stroke(color=RED)
-        # Position the cap on top of the sphere at the correct height
-        cap.shift(UP*(R-h))
-
-        # Text and formulas to display
+        # Display text and formulas
         volume_text = MathTex(r"V = \frac{1}{3}\pi h^2 (3R - h)").to_edge(UP)
-        volume_formula_crater = MathTex(r"V = \frac{1}{6}\pi h (3a^2 + h^2)").next_to(volume_text, DOWN)
+        volume_formula = MathTex(r"V = \frac{1}{6}\pi h (3a^2 + h^2)").next_to(volume_text, DOWN)
 
-        # Animate the sphere and cap creation
+        # Animate the creation of the sphere and the cap
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.begin_ambient_camera_rotation(rate=0.1)  # Slowly rotate the camera
-        self.play(ShowCreation(sphere))
-        self.play(ShowCreation(cap))
-        self.wait(2)
+        self.play(FadeIn(sphere), ShowCreation(cap_base))
+        self.wait(1)
+        self.play(TransformFromCopy(cap_base, cap_volume))
+        self.wait(1)
 
         # Show the volume formulas
-        self.play(Write(volume_text), Write(volume_formula_crater))
-        self.wait(2)
-
-        # Animate the cap to show it fits into the sphere
-        self.play(cap.animate.shift(DOWN*(R-h)))
-        self.wait(2)
-        self.play(cap.animate.shift(UP*(R-h)))
+        self.play(Write(volume_text), Write(volume_formula))
         self.wait(2)
 
         # Clear the scene
-        self.play(FadeOut(sphere), FadeOut(cap), FadeOut(volume_text), FadeOut(volume_formula_crater))
-        self.wait(2)
-
+        self.play(FadeOut(sphere), FadeOut(cap_base), FadeOut(cap_volume), FadeOut(volume_text), FadeOut(volume_formula))
+        self.wait(1)
