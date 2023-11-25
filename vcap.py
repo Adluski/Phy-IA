b5e9b0e9afe51dc6501f297a32ce7bc7a26f7ccd
Adit Mohan Bakshi <93762469+Adluski@users.noreply.github.com>
2023-11-25 19:12:32 +0530

    Update vcap.py

--- vcap.py
+++ vcap.py
@@ -7,26 +7,38 @@ class SphericalCapVolume(ThreeDScene):
         h = 1  # Height of the spherical cap
         a = (R**2 - (R - h)**2)**0.5  # Radius of the base of the spherical cap
         
-        # Create the sphere with radius R
+        # Create the full sphere with radius R
         sphere = Sphere(radius=R).set_opacity(0.5).set_color(BLUE)
+
+        # Create the spherical cap by slicing the sphere in half
+        cap = Sphere(radius=R).set_opacity(0.5).set_color(RED)
+        cap_mesh = SurfaceMesh(cap)
+        cap_mesh.set_stroke(width=0.5).set_color(RED)
         
-        # Create a hemisphere by scaling a sphere
-        hemisphere = Sphere(radius=R, u_min=PI/2, u_max=PI).set_opacity(0.5).set_color(RED)
-        hemisphere_bottom = Circle(radius=a).shift(DOWN*(R-h))
-        hemisphere.add_to_back(hemisphere_bottom)
+        # Function to remove the bottom half of the sphere's points
+        def remove_bottom_half(points):
+            for point in points:
+                if point[2] < 0:  # If the z-coordinate is below 0
+                    point[2] = 0  # Set z-coordinate to 0 (slice the sphere)
+            return points
+        
+        # Apply the function to the sphere mesh to create a hemisphere
+        cap_points = cap_mesh.get_all_points()
+        new_points = remove_bottom_half(cap_points)
+        cap_mesh.set_points(new_points)
 
-        # Position the hemisphere correctly within the sphere as a cap
-        hemisphere.shift(UP*h)
+        # Position the hemisphere correctly as a cap
+        cap.shift(UP*h)
 
-        # Text and formulas to display
+        # Display the text and formulas
         volume_text = MathTex(r"V = \frac{1}{3}\pi h^2 (3R - h)").to_corner(UL)
         volume_formula_crater = MathTex(r"V = \frac{1}{6}\pi h (3a^2 + h^2)").next_to(volume_text, DOWN)
 
-        # Animate the sphere and hemisphere creation
+        # Animate the creation and transformation of the sphere and cap
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.begin_ambient_camera_rotation(rate=0.1)  # Slowly rotate the camera
+        self.begin_ambient_camera_rotation(rate=0.1)
         self.add(sphere)
-        self.play(ShowCreation(hemisphere))
+        self.play(Create(cap_mesh))
         self.wait(2)
 
         # Show the volume formulas
@@ -34,16 +46,12 @@ class SphericalCapVolume(ThreeDScene):
         self.wait(2)
 
         # Animate the equivalence of the volumes
-        self.play(Transform(hemisphere, sphere))
+        self.play(Transform(cap_mesh, sphere))
         self.wait(2)
 
-        # Remove the formulas and stop the rotation
-        self.play(FadeOut(volume_text), FadeOut(volume_formula_crater))
-        self.stop_ambient_camera_rotation()
-        self.wait(2)
-        
         # Clear the scene
-        self.play(FadeOut(sphere), FadeOut(hemisphere))
+        self.play(FadeOut(sphere), FadeOut(cap_mesh), FadeOut(volume_text), FadeOut(volume_formula_crater))
+        self.wait(2)
 
 # To run this scene, use the following command in your terminal:
 # manim -pql vcap.py SphericalCapVolume
